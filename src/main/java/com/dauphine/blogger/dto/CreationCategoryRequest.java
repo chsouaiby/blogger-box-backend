e6e76dc5e7888ecb22ca093ded407fe09ad1f1cb
21102782 <christina.souaiby@etu.sorbonne-universite.fr>
2025-03-21 16:46:01 +0100

    4THCOMMIT

---
+++ src/main/java/com/dauphine/blogger/dto/CreationCategoryRequest.java
@@ -0,0 +1,15 @@
+package com.dauphine.blogger.dto;
+
+// DTO = Data Transfer Object
+public class CategoryRequest {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
